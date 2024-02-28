d5423672bf6e220786144054a8b95a70fed12cff
Jari Maijenburg <jari.maijenburg@formelio.nl>
2024-02-28 16:05:30 +0100

    Added populate_reference_targets tests

--- crates/fhir-sdk/src/client/r5/references.rs
+++ crates/fhir-sdk/src/client/r5/references.rs
@@ -39,3 +39,143 @@ pub fn populate_reference_targets<R: DomainResource>(
 		}
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	use fhir_model::r5::{
+		codes::{BundleType, ObservationStatus},
+		local_reference_to, reference_to,
+		resources::{
+			Bundle, BundleEntry, Observation, ObservationSubjectReferenceTarget, Patient, Resource,
+		},
+		types::{CodeableConcept, Reference},
+	};
+	use uuid::Uuid;
+
+	const BASE_URL: &'static str = "http://my.fhir.server/fhir";
+
+	#[test]
+	fn populates_references_local() {
+		let patient = Patient::builder().id(Uuid::new_v4().to_string()).build().unwrap();
+
+		let subject = local_reference_to(&patient).unwrap();
+
+		let mut observation = test_observation();
+		observation.contained = vec![patient.clone().into()];
+		observation.subject = Some(subject.into());
+
+		let bundle = test_bundle(vec![]);
+
+		populate_reference_targets(BASE_URL, &bundle, &mut observation);
+
+		assert_eq!(observation.contained, vec![Resource::Patient(patient)]);
+	}
+
+	#[test]
+	fn populates_references_urn() {
+		let patient_urn = Uuid::new_v4().urn().to_string();
+		let subject = Reference::builder().reference(patient_urn.clone()).build().unwrap();
+
+		let patient = test_patient();
+
+		let mut observation = test_observation();
+		observation.subject = Some(subject.into());
+
+		let observation_ref = reference_to(&observation).unwrap();
+		let observation_full_url =
+			observation_ref.parse().unwrap().with_base_url(BASE_URL).to_string();
+
+		let bundle = test_bundle(vec![
+			(&patient_urn, patient.clone().into()),
+			(&observation_full_url, observation.clone().into()),
+		]);
+
+		populate_reference_targets(BASE_URL, &bundle, &mut observation);
+
+		assert_eq!(
+			observation.subject.as_ref().and_then(|s| s.target.as_ref()),
+			Some(&Box::new(ObservationSubjectReferenceTarget::Patient(patient)))
+		);
+	}
+
+	#[test]
+	fn populates_references_absolute() {
+		let patient = test_patient();
+		let patient_ref = reference_to(&patient).unwrap();
+		let patient_full_url = patient_ref.parse().unwrap().with_base_url(BASE_URL);
+
+		let mut observation = test_observation();
+		observation.subject = Some(Reference::from(patient_full_url).into());
+
+		let observation_ref = reference_to(&observation).unwrap();
+		let observation_full_url = observation_ref.parse().unwrap().with_base_url(BASE_URL);
+
+		let bundle = test_bundle(vec![
+			(&patient_full_url.to_string(), patient.clone().into()),
+			(&observation_full_url.to_string(), observation.clone().into()),
+		]);
+
+		populate_reference_targets(BASE_URL, &bundle, &mut observation);
+
+		assert_eq!(
+			observation.subject.as_ref().and_then(|s| s.target.as_ref()),
+			Some(&Box::new(ObservationSubjectReferenceTarget::Patient(patient)))
+		);
+	}
+
+	#[test]
+	fn populates_references_relative() {
+		let patient = test_patient();
+		let patient_ref = reference_to(&patient).unwrap();
+		let patient_full_url = patient_ref.parse().unwrap().with_base_url(BASE_URL);
+
+		let mut observation = test_observation();
+		observation.subject = Some(Reference::from(patient_ref.clone()).into());
+
+		let observation_ref = reference_to(&observation).unwrap();
+		let observation_full_url = observation_ref.parse().unwrap().with_base_url(BASE_URL);
+
+		let bundle = test_bundle(vec![
+			(&patient_full_url.to_string(), patient.clone().into()),
+			(&observation_full_url.to_string(), observation.clone().into()),
+		]);
+
+		populate_reference_targets(BASE_URL, &bundle, &mut observation);
+
+		assert_eq!(
+			observation.subject.as_ref().and_then(|s| s.target.as_ref()),
+			Some(&Box::new(ObservationSubjectReferenceTarget::Patient(patient)))
+		);
+	}
+
+	fn test_bundle(resources: Vec<(&str, Resource)>) -> Bundle {
+		let mut bundle = Bundle::builder().r#type(BundleType::Collection).build().unwrap();
+
+		for (full_url, resource) in resources {
+			let entry = BundleEntry::builder()
+				.full_url(full_url.to_string())
+				.resource(resource)
+				.build()
+				.unwrap();
+
+			bundle.entry.push(Some(entry));
+		}
+
+		bundle
+	}
+
+	fn test_patient() -> Patient {
+		Patient::builder().id(Uuid::new_v4().to_string()).build().unwrap()
+	}
+
+	fn test_observation() -> Observation {
+		Observation::builder()
+			.id(Uuid::new_v4().to_string())
+			.status(ObservationStatus::Final)
+			.code(CodeableConcept::builder().build().unwrap())
+			.build()
+			.unwrap()
+	}
+}
