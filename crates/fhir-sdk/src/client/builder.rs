cb659492c242582ee0857f3e6210fa19408db712
Jari Maijenburg <jari.maijenburg@formelio.nl>
2024-07-25 15:21:26 +0200

    Added option to set a custom `reqwest::Client` to the ClientBuilder

--- crates/fhir-sdk/src/client/builder.rs
+++ crates/fhir-sdk/src/client/builder.rs
@@ -11,14 +11,19 @@ use reqwest::{header::HeaderValue, Url};
 use super::{AuthCallback, Client, Error, RequestSettings};
 
 /// Default user agent of this client.
-const DEFAULT_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
+const DEFAULT_USER_AGENT: HeaderValue =
+	HeaderValue::from_static(concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION")));
 
 /// Builder for the [Client]
 pub struct ClientBuilder<Version = super::DefaultVersion> {
 	/// The FHIR server's base URL.
 	base_url: Option<Url>,
 	/// User agent to use for requests.
-	user_agent: Option<String>,
+	/// If a client is set using [`ClientBuilder::client`], this value will
+	/// override the user agent value in the [reqwest::Client]
+	user_agent: Option<HeaderValue>,
+	/// [reqwest::Client] to use for all requests
+	client: Option<reqwest::Client>,
 	/// Request settings.
 	request_settings: Option<RequestSettings>,
 	/// Auth callback.
@@ -32,6 +37,7 @@ impl<V> Default for ClientBuilder<V> {
 		Self {
 			base_url: None,
 			user_agent: None,
+			client: None,
 			request_settings: None,
 			auth_callback: None,
 			version: PhantomData,
@@ -49,8 +55,15 @@ impl<V> ClientBuilder<V> {
 
 	/// User agent to use for requests.
 	#[must_use]
-	pub fn user_agent(mut self, user_agent: String) -> Self {
-		self.user_agent = Some(user_agent);
+	pub fn user_agent(mut self, user_agent: impl Into<HeaderValue>) -> Self {
+		self.user_agent = Some(user_agent.into());
+		self
+	}
+
+	/// [reqwest::Client] to use
+	#[must_use]
+	pub fn client(mut self, client: reqwest::Client) -> Self {
+		self.client = Some(client);
 		self
 	}
 
@@ -85,10 +98,14 @@ impl<V> ClientBuilder<V> {
 			return Err(Error::UrlCannotBeBase);
 		}
 
-		let user_agent = self.user_agent.as_deref().unwrap_or(DEFAULT_USER_AGENT);
-		let client = reqwest::Client::builder().user_agent(user_agent).build()?;
+		let user_agent = self.user_agent.unwrap_or(DEFAULT_USER_AGENT);
 
-		let request_settings = self.request_settings.unwrap_or_default();
+		let client = self.client.unwrap_or_default();
+
+		let request_settings = self
+			.request_settings
+			.unwrap_or_default()
+			.header(reqwest::header::USER_AGENT, user_agent);
 
 		let data = super::ClientData {
 			base_url,
@@ -105,6 +122,7 @@ impl<V> Clone for ClientBuilder<V> {
 		Self {
 			base_url: self.base_url.clone(),
 			user_agent: self.user_agent.clone(),
+			client: self.client.clone(),
 			request_settings: self.request_settings.clone(),
 			auth_callback: self.auth_callback.clone(),
 			version: self.version,
@@ -117,6 +135,7 @@ impl<V> std::fmt::Debug for ClientBuilder<V> {
 		f.debug_struct("ClientBuilder")
 			.field("base_url", &self.base_url)
 			.field("user_agent", &self.user_agent)
+			.field("client", &self.client)
 			.field("request_settings", &self.request_settings)
 			.field("auth_callback", &self.auth_callback.as_ref().map(|_| "<fn>"))
 			.finish()
